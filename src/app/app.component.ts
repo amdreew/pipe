afe8c59519d7463228e2a721bbd2e409747edad7
andres <mospinog21@curnvirtual.edu.co>
2020-01-14 18:02:14 -0500

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { resolve } from 'dns';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipe';
+  nombre = 'andres';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  pi = Math.PI;
+
+  n = 0.234;
+  salario = 12345;
+
+  heroe = {
+    nombre: 'iroman',
+    codigo: 'iron',
+    cargo: 'jefe',
+    direccion: {
+      calle: 'avenida primera',
+      casa: 'casa numero 7'
+    }
+  };
+
+
+  // tslint:disable-next-line:no-shadowed-variable
+  valorDePromesa = new Promise(  ( resolve , reject ) => {
+
+    setTimeout( () => resolve('LLego la data!'), 3500 );
+
+  });
 }
